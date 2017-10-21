277dff0e330b4a4fb724d99e2aa0c24baa3473f0
Villa-Reichswald <niklas0607@t-online.de>
2017-10-21 18:11:21 +0200

    Website

---
+++ Suche.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="de">
+
+    
+    
+<head>
+
+    
+    
+    
+    <meta charset="utf-8">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Aktivitätsvorschläge</title>
+    
+
+
+    <link href="Suche.css" rel="stylesheet"/>
+    
+    
+    
+</head>
+
+    
+    
+    
+    
+<body id="page-top">
+    
+
+    
+    
+    
+    
+    
+<!--    Suchleiste-->
+    
+<!--
+    
+   <div id="output"></div>
+    
+
+    <div id="suchform">
+        <form  id="search" method="get" action="Neu.html"   onsubmit="return showWait();">
+
+            <p>
+                <label for="suchbegriff">Suchen:</label>
+
+                <input type="text" name="q" id="suchbegriff" value="Suchbegriff" size="13" title="" onblur="if(this.value=='')this.value='Suchbegriff';" onfocus="if(this.value=='Suchbegriff')this.value='';" />
+
+                <input type="submit" value="Suchen !" />
+
+   
+
+            </p>
+        </form>
+    </div>
+    
+    
+-->
+           
+    <h><strong>Wie alt ist ihr jüngstes Mitglied?</strong></h>
+    
+    <br/><br/>
+    
+ 
+    <a href="Alter.html" class="btn btn-primary"><strong>Unter 12 Jahren</strong></a>
+    <a href="Alter2.html" class="btn btn-primary"><strong>Über 12 Jahren</strong></a>
+   
+
+    
+   <br/><br/>
+    
+    
+   <img src="dynamic%5B1%5D.jpg" class="img-responsive" alt="Bild von drei Unterschiedlichen Kindern">
+ 
+
+    
+    
+    
+
+</body>
+    
+</html>    
